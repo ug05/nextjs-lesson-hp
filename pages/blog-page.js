56936071303ab234236ba3778b84d696a4f17700
tryt-me-yamamoto <me-yamamoto@tryt-group.co.jp>
2021-11-24 17:57:34 +0900

    changed to function

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -3,15 +3,13 @@ import Layout from "../components/Layout";
 import Post from "../components/Post";
 import { getAllPostsData } from "../lib/posts";
 
-const Blog = ({ posts }) => {
+export default function Blog({ posts }) {
     return <Layout title="Blog">
         <ul className="m-10">
             {posts && posts.map((post) => <Post key={post.id} post={post} />)}
         </ul>
     </Layout>;
-};
-
-export default Blog;
+}
 
 export async function getStaticProps() {
     const posts = await getAllPostsData();
--- pages/contact-page.js
+++ pages/contact-page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 import Layout from "../components/Layout";
 
-const Contact = () => {
+export default function Contact() {
     return <Layout title="Contact">
         <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
             <div className="mt-4">
@@ -72,6 +72,4 @@ const Contact = () => {
             </div>
         </div>
     </Layout>;
-};
-
-export default Contact;
+}
